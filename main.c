b05bbce1a61493c446d6e58052dda8b0ecce6595
CxrlosKenobi <kenobi@ieee.org>
2022-07-14 11:33:15 -0400

    Add base controller modules

--- main.c
+++ main.c
@@ -1,28 +1,169 @@
 #include <stdio.h>
 #include <SDL2/SDL.h>
-#include <math.h>
+#include <SDL2/SDL_image.h>
 
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
-const double FPS = 60;
-const int time = 3;
+#define WINDOW_WIDTH (640)
+#define WINDOW_HEIGHT (480)
+#define SPEED (300)
 
-int main () {
-	SDL_Window* window = NULL;
-	SDL_Surface* screenSurface = NULL;
 
-	if (SDL_Init(SDL_INIT_VIDEO) >= 0) { // main logic of the game
-		window = SDL_CreateWindow("Our first game test :D", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-		if (window != NULL) {
-			screenSurface = SDL_GetWindowSurface(window);
-			SDL_UpdateWindowSurface(window);
-			SDL_Delay(2000);
-		} else
-			printf("The window could not be created correctly :/\nSDL_ERROR: %s\n", SDL_GetError());
-	} else
-		printf("The Video could not run correctly\nSDL_ERROR: %s\n", SDL_GetError());
+int main (void) {
+  if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER) != 0) {
+    printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+    return 1;
+  }
 
-	SDL_DestroyWindow(window);
-	SDL_Quit();
-	return 0;
+  SDL_Window *gameWindow = SDL_CreateWindow(
+    "WASD & Arrows",
+    SDL_WINDOWPOS_CENTERED,
+    SDL_WINDOWPOS_CENTERED,
+    WINDOW_WIDTH,
+    WINDOW_HEIGHT, 0
+    );
+
+    Uint32 renderFlags = SDL_RENDERER_ACCELERATED;
+    SDL_Renderer *gameRenderer = SDL_CreateRenderer(gameWindow, -1, renderFlags);
+
+    if (!gameRenderer) {
+      printf("Renderer could not be created! SDL_Error: %s\n", SDL_GetError());
+      SDL_DestroyWindow(gameWindow);
+      SDL_Quit();
+      return 1;
+    }
+
+    SDL_Surface *spriteSurface = IMG_Load("assets/dude.png");
+    if (!spriteSurface) {
+      printf("Error: %s\n", IMG_GetError());
+      SDL_DestroyRenderer(gameRenderer);
+      SDL_DestroyWindow(gameWindow);
+      SDL_Quit();
+      return 1;
+    }
+
+    SDL_Texture *spriteTexture = SDL_CreateTextureFromSurface(gameRenderer, spriteSurface);
+    SDL_FreeSurface(spriteSurface);
+
+    if (!spriteTexture) {
+      printf("Error: %s\n", SDL_GetError());
+      SDL_DestroyRenderer(gameRenderer);
+      SDL_DestroyWindow(gameWindow);
+      SDL_Quit();
+      return 1;
+    }
+
+    SDL_Rect spriteRectangle;
+    SDL_QueryTexture(
+      spriteTexture,
+      NULL, NULL,
+      &spriteRectangle.w,
+      &spriteRectangle.h
+    );
+
+    spriteRectangle.w /= 4;
+    spriteRectangle.h /= 4;
+
+    float xPosition = WINDOW_WIDTH / 2 - spriteRectangle.w / 2;
+    float yPosition = WINDOW_HEIGHT / 2 - spriteRectangle.h / 2;
+    float xSpeed = 0, ySpeed = 0;
+
+    int up = 0, down = 0, left = 0, right = 0;
+    int closeWindow = 0;
+    while (closeWindow == 0) {
+      SDL_Event gameEvent;
+      while (SDL_PollEvent(&gameEvent)) {
+        switch (gameEvent.type) {
+          case SDL_QUIT:
+            closeWindow = 1;
+            break;
+          
+          case SDL_KEYDOWN:
+            switch (gameEvent.key.keysym.scancode) {
+              case SDL_SCANCODE_UP:
+              case SDL_SCANCODE_W:
+                up = 1;
+                break;
+              case SDL_SCANCODE_DOWN:
+              case SDL_SCANCODE_S:
+                down = 1;
+                break;
+              case SDL_SCANCODE_LEFT:
+              case SDL_SCANCODE_A:
+                left = 1;
+                break;
+              case SDL_SCANCODE_RIGHT:
+              case SDL_SCANCODE_D:
+                right = 1;
+                break;
+              default:
+                break;
+            }
+            break;
+
+          case SDL_KEYUP:
+            switch (gameEvent.key.keysym.scancode) {
+              case SDL_SCANCODE_UP:
+              case SDL_SCANCODE_W:
+                up = 0;
+                break;
+              case SDL_SCANCODE_DOWN:
+              case SDL_SCANCODE_S:
+                down = 0;
+                break;
+              case SDL_SCANCODE_LEFT:
+              case SDL_SCANCODE_A:
+                left = 0;
+                break;
+              case SDL_SCANCODE_RIGHT:
+              case SDL_SCANCODE_D:
+                right = 0;
+                break;
+              default:
+                break;
+            }
+            break;
+          
+          default:
+            break;
+        }
+      }
+
+      xSpeed = 0; ySpeed = 0;
+      if (up == 1 && down == 0)
+        ySpeed = -SPEED;
+      if (down == 1 && up == 0)
+        ySpeed = SPEED;
+      if (left == 1 && right == 0)
+        xSpeed = -SPEED;
+      if (right == 1 && left == 0)
+        xSpeed = SPEED;
+
+      xPosition += xSpeed / 60;
+      yPosition += ySpeed / 60;
+
+      spriteRectangle.x = (int)xPosition;
+      spriteRectangle.y = (int)yPosition;
+
+      // Keep player sprite in the bounds of the window
+      if (xPosition <= 0)
+        xPosition = 0;
+      if (yPosition <= 0)
+        yPosition = 0;
+      if (xPosition >= (WINDOW_WIDTH - spriteRectangle.w))
+        xPosition = WINDOW_WIDTH - spriteRectangle.w;
+      if (yPosition >= (WINDOW_HEIGHT - spriteRectangle.h))
+        yPosition = WINDOW_HEIGHT - spriteRectangle.h;
+      
+      SDL_RenderClear(gameRenderer);
+      SDL_RenderCopy(gameRenderer, spriteTexture, NULL, &spriteRectangle);
+      SDL_RenderPresent(gameRenderer);
+
+      SDL_Delay(1000 / 60);
+    }
+
+  SDL_DestroyTexture(spriteTexture);
+  SDL_DestroyRenderer(gameRenderer);
+  SDL_DestroyWindow(gameWindow);
+  SDL_Quit();
+
+  return 0;
 }
---
+++ run.sh
@@ -0,0 +1,24 @@
+###
+# Run.sh -- Our cool shortcut to run SDL2 packages at once.
+###
+# runner="gcc main.c -o Breakout -lSDL2 -lSDL2_image";
+runner="gcc main.c -o Breakout -lSDL2 -lSDL2_image";
+ 
+clear;
+echo -e "\e[35m[ \e[33m-\e[90m ] \e[33mCompiling ... \e[35m";
+
+echo -e "\e[90m[ $ ] $runner \n\e[35m";
+eval $runner;
+
+if [ $? -eq 0 ]; then
+  echo -e "[ \e[32m+\e[90m ] \e[0mCompiled succesfully! \e[0m";
+  echo -e "\e[35m[ \e[33m-\e[90m ] \e[33mRunning ... \e[35m";
+  ./Breakout;
+else
+  echo -e "\n[ \e[31m!\e[90m ] \e[31mCompilation failed. \e[35m";
+
+fi;
+
+echo -e "\n[ \e[32m+\e[90m ] \e[0mClosed. \e[0m";
+exit 0;
+
