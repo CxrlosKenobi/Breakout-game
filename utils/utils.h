d775b634059abc5d0f02f217147b9bd844f19341
CxrlosKenobi <kenobi@ieee.org>
2022-07-16 16:04:34 -0400

    Add *untested* utils modules

--- utils/utils.h
+++ utils/utils.h
@@ -1,5 +1,29 @@
 #include <stdlib.h>
-#include <SDL2/SDL.h>
+#include <stdbool.h>
+#include <SDL2/SDL_image.h>
+
+
+bool LoadSurfaceTextureIMG(
+	const char *filePath,
+	SDL_Surface **objSurface, 
+	SDL_Texture **objTexture,
+	SDL_Renderer **gRenderer
+	) {
+
+	*objSurface = IMG_Load(filePath);
+	if (!*objSurface) {
+		printf("Error[-1]: %s\n", IMG_GetError());
+		return false;
+	}
+
+	*objTexture = SDL_CreateTextureFromSurface(*gRenderer, *objSurface);
+	if (!*objTexture) {
+		printf("Error[-1]: %s\n", SDL_GetError());
+		return false;
+	}
+}
+
+
 
 void destroyAll(SDL_Window *window, SDL_Renderer *renderer, SDL_Surface *surface) {
 	if (window != NULL)
