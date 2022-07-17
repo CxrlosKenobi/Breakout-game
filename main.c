73ab164190182ecd14683bd881c690f982e27cb4
AlbFR <albertoferrada20@gmail.com>
2022-07-17 19:26:28 -0400

    Background theme added and bricks rendered

--- main.c
+++ main.c
@@ -9,6 +9,7 @@
 #include "modules/structs.h"
 #include "modules/Paddle/init.h"
 #include "modules/CollidingBall/CollidingBall.h"
+#include "modules/Bricks/Bricks.h"
 #include "utils/utils.h"
 
 /* Global Settings */
@@ -22,18 +23,36 @@ const unsigned short WINDOW_HEIGHT = 480;
 SDL_Window *gWindow = NULL;
 SDL_Renderer *gRenderer = NULL;
 
-int main()
-{
+int main() {
   bool start = Initialize("Breakout!", WINDOW_WIDTH, WINDOW_HEIGHT, &gWindow, &gRenderer);
   if (!start)
     return 1;
 
+  SDL_Surface *bgSurface = IMG_Load("assets/sprites/fondoOscuro.png");
+  SDL_Texture *bgTexture = SDL_CreateTextureFromSurface(gRenderer, bgSurface);
+
+  // Bricks setup
+  const unsigned short rows = 8;
+  const unsigned short cols = 16;
+  Brick **bricks = createRandomBrickMatrix(rows, cols);
+  SDL_Surface *brickSurface[4];
+  brickSurface[0] =  NULL;
+  brickSurface[1] =  IMG_Load("assets/sprites/bloqueNaranja.png");
+  brickSurface[2] =  IMG_Load("assets/sprites/bloqueVerde.png");
+  brickSurface[3] =  IMG_Load("assets/sprites/bloqueAzul.png");
+  SDL_Texture *brickTextures[4];
+  brickTextures[1] = SDL_CreateTextureFromSurface(gRenderer, brickSurface[1]);
+  brickTextures[2] = SDL_CreateTextureFromSurface(gRenderer, brickSurface[2]);
+  brickTextures[3] = SDL_CreateTextureFromSurface(gRenderer, brickSurface[3]);
+
   // Ball setup
   Ball *b = malloc(sizeof(Ball));
+  unsigned short ballsAmount = 1;
   initBall(b, WINDOW_WIDTH, WINDOW_HEIGHT);
   SDL_Surface *ballSurface = IMG_Load("assets/sprites/bola.png");
   SDL_Texture *ballTexture = SDL_CreateTextureFromSurface(gRenderer, ballSurface);
 
+  // Paddle setup
   Paddle paddle;
   bool summon_paddle = initPaddle(
       &paddle,
@@ -143,16 +162,25 @@ int main()
     if (paddle.yPos >= (WINDOW_HEIGHT - paddle.rect.h))
       paddle.yPos = WINDOW_HEIGHT - paddle.rect.h;
 
-		updateBalls(b, 1, &closeWindow, WINDOW_WIDTH, WINDOW_HEIGHT, paddle);
+		// updateBalls(b, 1, &closeWindow, WINDOW_WIDTH, WINDOW_HEIGHT, paddle);
+    for (unsigned short i=0;i<ballsAmount;++i) {
+      manageWallCollision(b+i, &closeWindow, WINDOW_WIDTH, WINDOW_HEIGHT);
+      managePaddleCollision(b+i, paddle);
+      // manageBricksCollision(b+i, bricks);
+      (b+i)->pos.x += (b+i)->vel.x;
+      (b+i)->pos.y += (b+i)->vel.y;
+    }
 
     SDL_RenderClear(gRenderer);
+    SDL_RenderCopy(gRenderer, bgTexture, NULL, NULL);
     SDL_RenderCopy(gRenderer, paddle.texture, NULL, &paddle.rect);
+    renderBricks(bricks, gRenderer, brickTextures, rows, cols, WINDOW_WIDTH, 2*WINDOW_HEIGHT/5);
     renderBall(*b, gRenderer, ballTexture);
     SDL_RenderPresent(gRenderer);
 
     SDL_Delay(1000 / FPS);
   }
-
+  freeBricks(bricks, rows);
   SDL_DestroyRenderer(gRenderer);
   SDL_DestroyWindow(gWindow);
   SDL_Quit();
---
+++ modules/Bricks/Bricks.h
@@ -0,0 +1,48 @@
+Brick** createRandomBrickMatrix (const unsigned short m, const unsigned short n) {
+	Brick **bricks = malloc(m * sizeof(Brick*));
+	for (unsigned short i=0;i<m;++i) {
+		bricks[i] = malloc(n * sizeof(Brick));
+    for (unsigned short j=0;j<n;++j) {
+      bricks[i][j].health = rand() % 4;
+      bricks[i][j].power = 0;
+    }
+	}
+  return bricks;
+}
+void freeBricks (Brick **bricks, const unsigned short m) {
+  for (unsigned short i=0;i<m;++i)
+    free(bricks[i]);
+  free(bricks);
+}
+
+void renderBricks (Brick **m, SDL_Renderer *renderer, SDL_Texture **texture, const unsigned short rows, const unsigned short cols, const unsigned short MATRIX_WIDTH, const unsigned short MATRIX_HEIGHT) {
+  SDL_Rect *rect = malloc(sizeof(SDL_Rect));
+  SDL_Texture *brickTexture;
+  rect -> w = MATRIX_WIDTH / cols - 2;
+  rect -> h = MATRIX_HEIGHT/ rows - 2;
+  rect -> y = 0;
+  for (unsigned short i=0;i<rows;++i) {
+    rect -> x = 0;
+    for (unsigned short j=0;j<cols;++j) {
+      if (!m[i][j].health)
+        continue;
+      switch (m[i][j].health) {
+        case 1:
+          brickTexture = texture[1];
+          break;
+        case 2:
+          brickTexture = texture[2];
+          break;
+        case 3:
+          brickTexture = texture[3];
+          break;
+        default:
+          printf("Something strange has happened with bricks health\n");
+          break;
+      }
+      SDL_RenderCopy(renderer, brickTexture, NULL, rect);
+      rect -> x += rect -> w + 4;
+    }
+    rect -> y += rect -> h + 4;
+  }
+}
